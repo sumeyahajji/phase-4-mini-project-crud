dcc4d8fbc44b748bc2b569fd2264554229a51160
+ <+>
2023-03-25 00:16:41 +0300

    done

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    def index
+        spice = Spice.all
+        render json: spice
+     end
+   def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+   end
+   def update
+    spice = Spice.find(params[:id])
+    if spice
+        spice.update(spice_params)
+        render json: spice, status: :accepted
+    else
+        render json: {error: "Spice not found"}, status: :not_found
+    end
+   end
+   def destroy
+    spice = Spice.find(params[:id])
+    if spice
+        spice.destroy
+        head :no_content
+    else
+        render json: {error: "Spice not found"}, status: :not_found
+    end
+   end
+   private
+   def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+   end
+end
